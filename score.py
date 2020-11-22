9dc60ff497c2b04801fd5b9d221d892287c359a7
Anh Tuan Chau <73784610+tuananhc@users.noreply.github.com>
2020-11-22 23:12:51 +0700

    Update score.py

--- score.py
+++ score.py
@@ -1,4 +1,4 @@
-def comp10001huxxy_score(cards):
+def huxxy_score(cards):
     '''The function takes a list of cards and calculates their sum'''
     valdict = {'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, 
                '7': 7, '8': 8, '9': 9, '0': 10, 'J': 11, 'Q': 12, 'K': 13}
